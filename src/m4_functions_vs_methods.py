9f9c4c0713ce602f953fa449b9fd28b94aedacc7
khatria <khatria@rose-hulman.edu>
2018-11-29 00:14:11 -0500

    M4 exercise proj 2

--- src/m4_functions_vs_methods.py
+++ src/m4_functions_vs_methods.py
@@ -20,6 +20,8 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 import rosegraphics as rg
 
 
+
+
 def main():
     """
     Makes a TurtleWindow, calls the other functions in this module
@@ -136,10 +138,16 @@ def try_methods():
       -- backward  100 units
     """
     ###########################################################################
-    # TODO: 4. Implement and test this function, per its doc-string above.
+    # DONE: 4. Implement and test this function, per its doc-string above.
     #   The testing code (in main) is already written for you.
     ###########################################################################
 
+    alec = rg.SimpleTurtle()
+    alec.pen = rg.Pen('brown',5)
+    alec.forward(150)
+    alec.left(90)
+    alec.right(50)
+    alec.backward(100)
 
 ###############################################################################
 # IMPORTANT: Read the NOTE below before you try to implement the next function!
@@ -152,7 +160,7 @@ def try_functions():
      -- One jumps to (-50, 50), then moves (while drawing) to (100, 100)
     """
     ###########################################################################
-    # TODO: 5. Implement and test this function, per its doc-string above.
+    # DONE: 5. Implement and test this function, per its doc-string above.
     #   The testing code (in main) is already written for you.
     #
     #    NOTE: This function requires
@@ -160,6 +168,16 @@ def try_functions():
     #    If you think it needs more, ** ASK FOR HELP. **
     #    HINT: see   jump_and_move_turtle   above.
     ###########################################################################
+popeye = rg.SimpleTurtle()
+jump_and_move_turtle(200, 100, 300, 30)
+
+swi = rg.SimpleTurtle()
+jump_and_move_turtle(100, 200, 0, 0)
+
+wolfy = rg.SimpleTurtle()
+jump_and_move_turtle(-50, 50, 100, 100)
+
+
 
 
 ###############################################################################
@@ -198,15 +216,28 @@ def try_methods_and_functions():
       8. Draw a SQUARE whose sides are each of length 50.
     """
     ###########################################################################
-    # TODO: 6. Implement and test this function, per its doc-string above.
+    # DONE: 6. Implement and test this function, per its doc-string above.
     #   The testing code (in main) is already written for you.
     #
     #   NOTE: This function should ** CALL ** the
     #     draw_many_squares
     #   function defined above.  If you don't see why, ** ASK FOR HELP. **
     ###########################################################################
-
-
+mike = rg.SimpleTurtle('turtle')
+mike.pen = rg.Pen('blue', 5)
+mike.backward(150)
+mike.speed = 1
+draw_many_squares(mike, 2, 100, 30)
+mike.speed = 5
+mike.pen = rg.Pen('red', 2)
+draw_many_squares(mike, 10, 50, 15)
+mike.speed = 100
+mike.pen = rg.Pen('green', 35)
+draw_many_squares(mike, 8, 300, 60)
+mike.pen = rg.Pen('black', 3)
+mike.backward(200)
+mike.draw_circle(30)
+mike.draw_square(50)
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
